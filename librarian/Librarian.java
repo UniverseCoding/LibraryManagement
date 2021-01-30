a2adcc532bbb0131732833f1102e91182dd130a9
salamaddin <salamaddinsk100@gmail.com>
2021-01-30 15:50:13 +0530

    2nd commit

--- librarian/Librarian.java
+++ librarian/Librarian.java
@@ -2,9 +2,7 @@
 package librarian;
 import dashboardDesign.*;
 public class Librarian {
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
         DashboardDesign dashObj=new DashboardDesign();
         dashObj.gui();
