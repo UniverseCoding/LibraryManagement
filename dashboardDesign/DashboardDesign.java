57b6764164491f8863733c25bf5dce025b531830
salamaddin <77258564+salamaddin@users.noreply.github.com>
2021-02-04 21:10:17 +0530

    JButton added

--- dashboardDesign/DashboardDesign.java
+++ dashboardDesign/DashboardDesign.java
@@ -17,7 +17,7 @@ import java.awt.geom.RoundRectangle2D;
 
 
 public class DashboardDesign extends JFrame{  // Container **
-    JButton b1,b2,b3,b4,b5,b6;
+    JButton b1,b2,b3,b4,b5,b6,b7,b8;
     JMenu file,edit,view;
     JMenuBar mb;
     JMenuItem save,saveas,saveall,exit;
@@ -75,25 +75,49 @@ public class DashboardDesign extends JFrame{  // Container **
 
     public void frameButton()
     {
+        // JButton add
         b1=new JButton("Check BOOK");
         b2=new JButton("Book Search"); 
         b3=new JButton();
         b4=new JButton();
         
+        b5=new JButton();
+        b6=new JButton();
+        b7=new JButton();
+        b8=new JButton();
+        
+        //JButton location set
         b1.setBounds(200,200, 130, 130);
         b2.setBounds(475,200, 130, 130);
         b3.setBounds(750,200, 130, 130);
         b4.setBounds(1025,200, 130, 130);
         
+        b5.setBounds(200,455, 130, 130);
+        b6.setBounds(475,455, 130, 130);
+        b7.setBounds(750,455, 130, 130);
+        b8.setBounds(1025,455, 130, 130);
+        
+        //JButton color add
         b1.setBackground(new Color(204, 255, 204));
         b2.setBackground(new Color(204, 255, 204));
         b3.setBackground(new Color(204, 255, 204));
         b4.setBackground(new Color(204, 255, 204));
         
+        b5.setBackground(new Color(204, 255, 204));
+        b6.setBackground(new Color(204, 255, 204));
+        b7.setBackground(new Color(204, 255, 204));
+        b8.setBackground(new Color(204, 255, 204));
+        
+        //JButton adding with JFrame
         add(b1);
         add(b2);
         add(b3);
         add(b4);
+        
+        add(b5);
+        add(b6);
+        add(b7);
+        add(b8);
     }
 
     @Override
