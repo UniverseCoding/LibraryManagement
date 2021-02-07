64761eb7731a2ca8ff2413165fd7ca39b6a04fea
Suman <60940736+imSmaity@users.noreply.github.com>
2021-02-07 13:30:27 +0530

    Add button icon

---
+++ dashboardDesign/IssueBook.java
@@ -0,0 +1,23 @@
+
+package dashboardDesign;
+
+import javax.swing.JFrame;
+
+public class IssueBook extends JFrame{
+
+    IssueBook()
+    {
+        setTitle("Library Management System");
+        setSize(1500,700);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setLayout(null);
+ 
+    }
+    public void gui()
+    {
+        //Code write here
+    }
+}
---
+++ dashboardDesign/StudentInformation.java
@@ -0,0 +1,23 @@
+
+package dashboardDesign;
+
+import javax.swing.JFrame;
+
+public class StudentInformation extends JFrame{
+    JFrame f1=new JFrame("New Book");
+
+    StudentInformation()
+    {
+        setSize(1500,700);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setLayout(null);
+ 
+    }
+    public void gui()
+    {
+        //Code write here
+    }
+}
