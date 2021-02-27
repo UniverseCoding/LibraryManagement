e24d25465eca255f87f8d6dda6b0d3ea864bc2c9
SoharabUddin <78019027+SoharabUddin@users.noreply.github.com>
2021-02-27 14:01:13 +0530

    Edited by Soharab
    Add construction and main method.

--- librarian/Librarian.java
+++ librarian/Librarian.java
@@ -3,12 +3,14 @@ package librarian;
 import dashboardDesign.*;
 
 public class Librarian {
-    public static void main(String[] args) {
+   
+    public Librarian(){
         DashboardDesign dashObj=new DashboardDesign();
         dashObj.menu();
         dashObj.frameButton();
         dashObj.setVisible(true);
-  }  
+    }
+    public static void main(String[] args) {
+        Librarian obj = new Librarian();
+    }  
 }
-
-
