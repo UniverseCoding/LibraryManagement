fc61b02a9946cfcfbce1558c8079738c13a5aea7
SoharabUddin <78019027+SoharabUddin@users.noreply.github.com>
2021-04-07 21:21:14 +0530

    Update NewAccount.java
    newAccount Updated by Soharab. 
    changes:
          ----> *Frame Background.
                  *JtextField and JPasswordField Background.
                  *Location of elements.

--- dashboardDesign/NewAccount.java
+++ dashboardDesign/NewAccount.java
@@ -29,7 +29,7 @@ import librarian.Librarian;
 
 public class NewAccount extends JFrame 
 {
-    JLabel l1,l2,l3;
+    JLabel label1,label2,label3;
     JTextField textField1;
     JPasswordField passwordField1,passwordField2;
     JButton submitButton,ClearButton,backButton;
@@ -42,6 +42,7 @@ public class NewAccount extends JFrame
     {
         setTitle("Library Management System");
         setSize(1500,700);
+        getContentPane().setBackground(new Color(190, 245, 104));
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -52,54 +53,58 @@ public class NewAccount extends JFrame
     public void gui ()
     {
        //Label Section.
-       l1 = new JLabel("Name: "); // create objects for Labels and given Names.
-       l2 = new JLabel("Password: ");
-       l3 = new JLabel("Re-Password: ");
-       l1.setBounds(172, 75, 120, 25); // set locations and size of labels.
-       l2.setBounds(172, 145, 180, 25);
-       l3.setBounds(172, 215, 230, 25);
-       l1.setForeground(Color.RED); //set  label's Text Color.
-       l2.setForeground(Color.GREEN);
-       l3.setForeground(Color.BLUE);
-       l1.setFont(new Font("Soharab", Font.PLAIN, 34)); //create new Font for Label.
-       l2.setFont(new Font("Soharab", Font.PLAIN, 34));
-       l3.setFont(new Font("Soharab", Font.PLAIN, 30));
-       add(l1); // add to JFrame.
-       add(l2);
-       add(l3);
+       label1 = new JLabel("Name: "); // create objects for Labels and given Names.
+       label2 = new JLabel("Password: ");
+       label3 = new JLabel("Re-Password: ");
+       label1.setBounds(572, 175, 120, 25); // set locations and size of labels.
+       label2.setBounds(572, 245, 180, 25);
+       label3.setBounds(572, 315, 230, 25);
+       label1.setForeground(Color.BLUE); //set  label's Text Color.
+       label2.setForeground(Color.BLUE);
+       label3.setForeground(Color.BLUE);
+       label1.setFont(new Font("Soharab", Font.PLAIN, 34)); //create new Font for Label.
+       label2.setFont(new Font("Soharab", Font.PLAIN, 34));
+       label3.setFont(new Font("Soharab", Font.PLAIN, 30));
+       add(label1); // add to JFrame.
+       add(label2);
+       add(label3);
        
        //TextField and PasswordField Section.
-       textField1 = new JTextField();  // create  objects for text field and password field.
+        // create  objects for text field and password field.
        passwordField1 = new JPasswordField(); 
+       textField1 = new JTextField(); 
        passwordField2 = new JPasswordField();
-       textField1.setFont(new Font("Soharab", Font.BOLD, 24)); // set Font and font Size.
+       // set Font and font Size.
+       textField1.setFont(new Font("Soharab", Font.BOLD, 24)); 
        passwordField1.setFont(new Font("Soharab", Font.ROMAN_BASELINE, 24));
        passwordField1.setEchoChar('*');
        passwordField2.setEchoChar('*');
        passwordField2.setFont(new Font("Soharab", Font.ROMAN_BASELINE, 24));
-       textField1.setBounds(372, 75, 460, 25); // set Location and Size.
-       passwordField1.setBounds(372, 145, 460, 25);
-       passwordField2.setBounds(372, 215, 460, 25);
-       Color color1 = new Color(12,177,80);//Create a color for Background.
-       textField1.setBackground(color1);
-       passwordField1.setBackground(color1);
-       passwordField2.setBackground(color1);
-       Color color2 = new Color(57,55,197);//Create a color for Forground.
+        // set Location and Size.
+       textField1.setBounds(772, 175, 460, 25);
+       passwordField1.setBounds(772, 245, 460, 25);
+       passwordField2.setBounds(772, 315, 460, 25);
+       //Create a color for Forground.
+       Color color2 = new Color(57,55,197);
        textField1.setForeground(color2);
        passwordField1.setForeground(color2);
        passwordField2.setForeground(color2);
-       add(textField1); // add to JFrame.
+       // add to JFrame.
+       add(textField1);
        add(passwordField1);
        add(passwordField2);
        
-       //Buttons Section.
-       submitButton =new JButton("Submit"); // create buttons object and gave a name.
+    //Buttons Section.
+        // create buttons object and gave a name.
+       submitButton =new JButton("Submit");
        ClearButton = new JButton("Clear All");
        backButton = new JButton();
-       submitButton.setBounds(372,315,210,35); // set location and size of Buttons.
-       ClearButton.setBounds(622,315,210,35);
-       backButton.setBounds(765,31,65,25);
-       add(submitButton); // add Buttons to Frame.
+       // set location and size of Buttons.
+       submitButton.setBounds(772,415,210,35); 
+       ClearButton.setBounds(1022,415,210,35);
+       backButton.setBounds(1165,131,65,25);
+        // add Buttons to Frame.
+       add(submitButton);
        add(ClearButton);
        add(backButton);
        submitButton.addActionListener(new ActionListener() {
@@ -150,7 +155,7 @@ public class NewAccount extends JFrame
         });
        //JCheckBox Section to visible Password.
        JCheckBox1 = new JCheckBox("Show");
-       JCheckBox1.setBounds(850, 145, 73, 25);
+       JCheckBox1.setBounds(1158, 275, 73, 25);
        add(JCheckBox1);
        JCheckBox1.addActionListener(new ActionListener(){
            public void actionPerformed(ActionEvent e){
@@ -164,7 +169,7 @@ public class NewAccount extends JFrame
        );
        
        JCheckBox2 = new JCheckBox("Show");
-       JCheckBox2.setBounds(850, 215, 73, 25);
+       JCheckBox2.setBounds(1158, 345, 73, 25);
        add(JCheckBox2);
        JCheckBox2.addActionListener(new ActionListener(){
            public void actionPerformed(ActionEvent e){
@@ -194,7 +199,7 @@ public class NewAccount extends JFrame
         Graphics2D g1 = (Graphics2D) g;
         g1.setPaint(new Color(0, 0, 0));
         g1.setStroke(new BasicStroke(2.0f));
-        g1.drawRoundRect(170, 63, 710, 360, 50, 50);  //50 and 50 is round size & x and y is position of rectangle
+        g1.drawRoundRect(570, 163, 710, 360, 50, 50);  //50 and 50 is round size & x and y is position of rectangle
      }
 }
 
