b49b74b87c50008372596b44dd4d2e30e6b2e4dc
Suman <60940736+imSmaity@users.noreply.github.com>
2021-05-18 12:12:21 +0530

    Update Librarian.java

--- librarian/Librarian.java
+++ librarian/Librarian.java
@@ -1,14 +1,12 @@
 
 package librarian;
-import dashboardDesign.*;
 
 public class Librarian {
    
     public Librarian(){
-        DashboardDesign dashObj=new DashboardDesign();
-        dashObj.menu();
-        dashObj.frameButton();
-        dashObj.setVisible(true);
+        Login login=new Login();
+        login.gui();
+        login.setVisible(true);
     }
     public static void main(String[] args) {
         Librarian obj = new Librarian();
